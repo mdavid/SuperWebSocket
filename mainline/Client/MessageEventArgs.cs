e1e53d46ef99778c8511abe77f03cd0e365156c2
SND\kerryjiang_cp <SND\kerryjiang_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2011-03-14 14:51:44 +0000

    changed folder WebSocketClient to Client
    git-svn-id: https://SuperWebSocket.svn.codeplex.com/svn@63690 81fbe566-5dc4-48c1-bdea-7421811ca204

--- mainline/Client/MessageEventArgs.cs
+++ mainline/Client/MessageEventArgs.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SuperWebSocket.WebSocketClient
+namespace SuperWebSocket.Client
 {
     public class MessageEventArgs : EventArgs
     {
