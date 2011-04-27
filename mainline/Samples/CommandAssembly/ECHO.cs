b789e4fd6851f6e20d075be571504baa4247923c
SND\kerryjiang_cp <SND\kerryjiang_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2011-04-27 05:00:06 +0000

    added command assembly sample project
    git-svn-id: https://SuperWebSocket.svn.codeplex.com/svn@66034 81fbe566-5dc4-48c1-bdea-7421811ca204

--- mainline/Samples/CommandAssembly/ECHO.cs
+++ mainline/Samples/CommandAssembly/ECHO.cs
@@ -5,7 +5,7 @@ using System.Text;
 using SuperWebSocket.SubProtocol;
 using SuperSocket.SocketBase.Command;
 
-namespace SuperWebSocket.Samples.SimpleSubProtocol.Command
+namespace SuperWebSocket.Samples.CommandAssembly
 {
     /// <summary>
     /// If client send "ECHO A B C" to server, the A, B and C will be sent back
